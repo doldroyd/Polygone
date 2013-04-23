2940ec899955485c8dff9ca948d9e7d3e461e292
Andrew Underwood <aruwx2@mst.edu>
2013-04-23 17:03:26 -0500

    fixed a typo

--- src/Sound.h
+++ src/Sound.h
@@ -14,15 +14,15 @@ class SoundSystem : public System
                                         sound4 = NULL; sound5 = NULL; sound6 = NULL; sound7 = NULL; sound8 = NULL; sound9 = NULL; soundn = NULL;/*all the sounds are null*/ }
   Mix_Music* music;
   Mix_Chunk* sound1;
-  Mox_Chunk* sound2;
-  Mox_Chunk* sound3;
-  Mox_Chunk* sound4;
-  Mox_Chunk* sound5;
-  Mox_Chunk* sound6;
-  Mox_Chunk* sound7;
-  Mox_Chunk* sound8;
-  Mox_Chunk* sound9;
-  Mox_Chunk* soundn;
+  Mix_Chunk* sound2;
+  Mix_Chunk* sound3;
+  Mix_Chunk* sound4;
+  Mix_Chunk* sound5;
+  Mix_Chunk* sound6;
+  Mix_Chunk* sound7;
+  Mix_Chunk* sound8;
+  Mix_Chunk* sound9;
+  Mix_Chunk* soundn;
 	  	  
   bool init();
   void update(std::chrono::duration time);
