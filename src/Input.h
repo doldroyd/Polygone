960c31312d49c62675b0e172fa754e5ee1cfba9a
Nate <ncdc86@mst.edu>
2013-04-23 15:35:57 -0500

    Input (incomplete)

---
+++ src/Input.h
@@ -0,0 +1,18 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <SDL.h>
+
+#include "System.h"
+
+#include "Position.h"
+
+class Input{
+	public:
+		Input();
+		~Input();
+
+		Key();
+		Mouse();
+};
+#endif
