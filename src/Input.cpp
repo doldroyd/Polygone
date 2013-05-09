be9ba5e76bcee0df489d47b650c4280704982fd6
Nate <ncdc86@mst.edu>
2013-05-08 22:42:50 -0500

    physics and input

--- src/Input.cpp
+++ src/Input.cpp
@@ -9,6 +9,7 @@ InputComponent* InputSystem::getEntity(int EntityID){
     auto i = input.find(EntityID);
 	if(i == input.end()){
 		input[EntityID].physicsCom = (PhysicsComponent*) Engine::instance().getSystem(PHYSICS_NAME)->getEntity(EntityID);
+		input[EntityID].positionCom = (PositionComponent*) Engine::instance().getSystem(POSITION_NAME)->getEntity(EntityID);
 	}
 	InputComponent* retval = &(input[EntityID]);
 	return retval;
@@ -53,14 +54,12 @@ void InputSystem::update(unsigned int delay){
 
 	//While the mouse is moving, constantly get its position
 	while(SDL_PeepEvents(&event, 1, SDL_GETEVENT, SDL_EVENTMASK(SDL_MOUSEMOTION))){
-		//mx = event.motion.x;
-		//my = event.motion.y;
 		//if in game then set mouse offset as player entity
 		//if( game == running){
 		//	second.positionCom->oldx = x;
-		//	input.begin()->second.positionCom->x = mx;
+		//	input.begin()->second.positionCom->x = event.motion.x;
 		//	second.positionCom->oldy = y;
-		//	input.begin()->second.positionCom->y = my;
+		//	input.begin()->second.positionCom->y = event.motion.y;
 		//}
 	}
 
@@ -71,7 +70,7 @@ void InputSystem::update(unsigned int delay){
         {
 			//If mouse offset is equal to the button offset, then do button down gif
             //If(mx == button.x && my == button.y){
-			//	button down gif nonexistent
+			//	button down gif nonexistent at the moment
 			//}
         }
     }
